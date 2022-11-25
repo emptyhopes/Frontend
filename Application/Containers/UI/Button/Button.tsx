29a2f9f6ea9817288e754125119332c6c28d3cc9
emptyhopes <literallymyfault@gmail.com>
2022-11-25 12:37:32 +0500

    fix stylelint errors

--- Application/Containers/UI/Button/Button.tsx
+++ Application/Containers/UI/Button/Button.tsx
@@ -5,13 +5,13 @@ import { FunctionComponent } from "react";
 import { ButtonInterface } from "@/Application/Containers/UI/Button/Types/ButtonInterface";
 
 const StyledButton = styled.button`
-  font-family: "IBM Plex Sans", sans-serif;
-  font-weight: 700;
   padding: 10px 20px;
   border: 1px solid #e0e3e7;
   border-radius: 10px;
   background: #ffffff;
   color: #3e5060;
+  font-family: "IBM Plex Sans", sans-serif;
+  font-weight: 700;
 
   &:hover {
     background: #e2edf8;
