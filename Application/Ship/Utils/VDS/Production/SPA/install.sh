d46dc98019a14586cdaece5aac951abd66b7f57c
emptyhopes <literallymyfault@gmail.com>
2022-11-30 14:50:00 +0500

    adding vds install and docker reduction

---
+++ Application/Ship/Utils/VDS/Production/SPA/install.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+sudo apt-get update --yes && sudo apt full-upgrade --yes
+
+sudo apt-get install nginx nodejs npm --yes
+
+temporary=$(mktemp --directory)
+
+git clone "https://github.com/emptyhopes/frontend.git" "$temporary/frontend"
+
+cd "$temporary/frontend"; npm install; npm run development
+
+sudo rm --recursive --force "/var/www/build"
+sudo rm --recursive --force "/etc/nginx/sites-available"
+sudo rm --recursive --force "/etc/nginx/sites-enabled"
+
+sudo mkdir --parents "/var/www/build"
+
+sudo mkdir --parents "/etc/nginx"
+sudo mkdir --parents "/etc/nginx/sites-available"
+sudo mkdir --parents "/etc/nginx/sites-enabled"
+
+sudo cp --recursive $temporary/frontend/Build/* "/var/www/build"
+sudo cp "$temporary/frontend/Application/Ship/Utils/Nginx/nginx.conf" "/etc/nginx/nginx.conf"
+sudo cp "$temporary/frontend/Application/Ship/Utils/Nginx/sites-available/localhost.conf" "/etc/nginx/sites-available/localhost.conf"
+
+sudo ln --symbolic "/etc/nginx/sites-available/localhost.conf" "/etc/nginx/sites-enabled/localhost.conf"
+
+sudo rm --recursive --force "$temporary"
+
+sudo nginx -t
+sudo systemctl restart nginx
---
+++ Application/Ship/Utils/VDS/Production/SSR/install.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+sudo apt-get update --yes && sudo apt full-upgrade --yes
+
+sudo apt-get install nodejs npm --yes
+
+temporary=$(mktemp --directory)
+
+git clone "https://github.com/emptyhopes/frontend.git" "$temporary/frontend"
+
+cd "$temporary/frontend"; npm install; npm run production:ssr
+
+sudo rm --recursive --force "/var/www/build"
+sudo mkdir --parents "/var/www/build"
+
+sudo cp --recursive $temporary/frontend/Build/* "/var/www/build"
+
+sudo rm --recursive --force "$temporary"
+
+node $(find /var/www/build -name server.*.js)
