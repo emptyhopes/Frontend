ce18817b93e5456073cc49b668495ea670b931e2
emptyhopes <literallymyfault@gmail.com>
2022-12-07 18:02:07 +0500

    adding utils vds install

--- Application/Ship/Utils/PWA/ServiceWorkerFunctions.ts
+++ Application/Ship/Utils/PWA/ServiceWorkerFunctions.ts
@@ -1,20 +1,20 @@
 /* eslint-disable no-console */
 
 const Register = () => {
-  window.addEventListener("load", async () => {
-    if (navigator) {
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", async () => {
       try {
         await navigator.serviceWorker.register("ServiceWorker.js");
       } catch (error) {
         console.log(error);
       }
-    }
-  });
+    });
+  }
 };
 
 const Unregister = () => {
-  window.addEventListener("load", async () => {
-    if (navigator) {
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", async () => {
       try {
         navigator.serviceWorker.getRegistration().then((registrations) => {
           for (const registration of [registrations]) {
@@ -24,8 +24,8 @@ const Unregister = () => {
       } catch (error) {
         console.log(error);
       }
-    }
-  });
+    });
+  }
 };
 
 export { Register, Unregister };
